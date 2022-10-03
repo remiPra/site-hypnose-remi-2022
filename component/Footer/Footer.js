81461aa5905bd7919dd89ff0881874bd7c73669b
remiPra <remipradere@gmail.com>
2022-10-03 16:32:11 +0200

    final projet

--- component/Footer/Footer.js
+++ component/Footer/Footer.js
@@ -4,13 +4,13 @@ import Link from 'next/link'
 function Footer() {
     return (
         <div className='block md:flex bg-[#1b271b] text-white  py-[50px] px-[50px]'>
-            <div className='block lg:flex'>
-            <div className='lg:w-1/2 min-w-[300px] py-0 '>
+            <div className='block  lg:flex lg:w-1/2 lg:justify-center'>
+            <div className='lg:w-1/2  py-0 '>
                 <h2 className='py-3 text-center'>Rémi PRADERE Hypnothérapeute</h2>
                 <h3 className='py-3'>Hypnose éricksonienne à Toulouse</h3>
                 <p>Le cabinet est facilement accessible depuis Blagnac, Balma, Ramonville-Saint-Agne, Colomiers, Saint Orens, Toulouse, Tournefeuille, Aucamville, Quint-Fonsegrives, L'Union, Beauzelle, Saint-Jean, Cugnaux ou Launaguet.</p>
             </div>
-            <div className='lg:w-1/2   py-0 px-[15px]'>
+            <div className='lg:w-1/2 max-w-[200px]   py-0 px-[15px]'>
                 <h3 className='py-3 text-center'>Plan du Site</h3>
                 <ul>
                     {[{ title: 'Accueil', href: '/' }, { title: 'Qui Suis je?', href: '/' }, { title: 'Contact', href: '/' }].map((el, index) => (
@@ -21,15 +21,15 @@ function Footer() {
                 </ul>
             </div>
             </div>
-            <div className='block lg:flex'>
-            <div className='lg:w-1/2 min-w-[300px]'>
+            <div className='block lg:flex lg:w-1/2  '>
+            <div className='lg:w-1/2 max-w-[200px]'>
                 <h3 className='py-3 text-center'>Réseaux Sociaux</h3>
             </div>
-            <div className='lg:w-1/2 min-w-[300px]'>
+            <div className='lg:w-1/2 max-w-[200px]'>
                 <h3 className='py-3 text-center'>Plan</h3>
                 <div className="mapouter">
                     <div className="gmap_canvas flex justify-center">
-                        <iframe width="250" height="100" id="gmap_canvas" src="https://maps.google.com/maps?q=cabinet%20hypnose%20remi%20pradere&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0">
+                        <iframe width="200" height="100" id="gmap_canvas" src="https://maps.google.com/maps?q=cabinet%20hypnose%20remi%20pradere&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0">
                         </iframe>
                     </div>
                 </div>
