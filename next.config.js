eb666c5eb35820189eb9c665ddcd7be0f43204f2
remiPra <remipradere@gmail.com>
2022-10-04 17:30:35 +0200

    recherche bug redondant

--- next.config.js
+++ next.config.js
@@ -5,9 +5,10 @@ const withPWA = require("next-pwa");
 module.exports = withPWA({
  
   pwa: {
-    dest: "public",
+    disable: process.env.NODE_ENV === 'development',
+    // dest: 'public', // comment out this line
     register: true,
-    skipWaiting: true,
+    sw: '/sw.js'
   },
 });
 
