cb15f124a59b575213e1a5019bf3ae2418250fd9
remiPra <remipradere@gmail.com>
2022-10-04 13:36:13 +0200

    sitemap

--- component/Drawer/Drawer.js
+++ component/Drawer/Drawer.js
@@ -4,6 +4,7 @@ import Drawer from '@mui/material/Drawer';
 import Button from '@mui/material/Button';
 import List from '@mui/material/List';
 import Divider from '@mui/material/Divider';
+import CottageIcon from '@mui/icons-material/Cottage';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
@@ -11,6 +12,8 @@ import ListItemText from '@mui/material/ListItemText';
 import InboxIcon from '@mui/icons-material/MoveToInbox';
 import MailIcon from '@mui/icons-material/Mail';
 import { AiOutlineMenu } from 'react-icons/ai'
+import Link from 'next/link';
+import { Cottage } from '@mui/icons-material';
 
 
 export default function TemporaryDrawer() {
@@ -30,14 +33,43 @@ export default function TemporaryDrawer() {
   };
 
   const list = (anchor) => (
-    <Box
-      sx={{ width: anchor === 'top' || anchor === 'bottom' ? 'auto' : 250 }}
-      role="presentation"
-      onClick={toggleDrawer(anchor, false)}
-      onKeyDown={toggleDrawer(anchor, false)}
-    >
-      <List>
-        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+    <>
+      <div className='my-2 mx-auto flex flex-col justify-center'>
+        <img style={{ margin: "5px auto" }} className="w-[100px]   md:w-[30%]" src='assets/logoRémiPradereSopromagnetiseur.png' />
+        <h1 className='text-center'>Rémi PRADERE <br />
+          Hypnothérapeute
+        </h1>
+      </div>
+      <Box
+        sx={{ width: anchor === 'top' || anchor === 'bottom' ? 'auto' : 250 }}
+        role="presentation"
+        onClick={toggleDrawer(anchor, false)}
+        onKeyDown={toggleDrawer(anchor, false)}
+      >
+        <List>
+          <nav>
+            <ul>
+             
+                  <Link passHref={true} href={"./"}>
+                    <li> <Cottage/>
+ 
+                      Accueil</li>
+                  </Link>
+                  <Link passHref={true} href={"./but"}>
+                    <li> 
+                  But de l'Hypnose
+                    </li>
+                  </Link>
+            
+
+
+            </ul>
+          </nav>
+
+        </List>
+        <Divider />
+        <List>
+          {/* {['All mail', 'Trash', 'Spam'].map((text, index) => (
           <ListItem key={text} disablePadding>
             <ListItemButton>
               <ListItemIcon>
@@ -46,22 +78,10 @@ export default function TemporaryDrawer() {
               <ListItemText primary={text} />
             </ListItemButton>
           </ListItem>
-        ))}
-      </List>
-      <Divider />
-      <List>
-        {['All mail', 'Trash', 'Spam'].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-    </Box>
+        ))} */}
+        </List>
+      </Box>
+    </>
   );
 
   return (
@@ -69,8 +89,8 @@ export default function TemporaryDrawer() {
       {['left'].map((anchor) => (
         <React.Fragment key={anchor}>
           <Button onClick={toggleDrawer(anchor, true)}>
-          
-          <AiOutlineMenu className='cursor-pointer' color='#1b271b' size={30} />
+
+            <AiOutlineMenu className='cursor-pointer' color='#1b271b' size={30} />
           </Button>
           <Drawer
             anchor={anchor}
---
+++ next-sitemap-config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    siteUrl: 'https://cabinet-hypnose-toulouse.netlify.app/',
+    generateRobotsTxt: true, // (optional)
+    // ...other options
+  }
